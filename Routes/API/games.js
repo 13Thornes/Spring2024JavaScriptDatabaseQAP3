d375c9661c5008c7845384b0bfd432cbf628842a
Samantha Thorne <150044477+13Thornes@users.noreply.github.com>
2024-07-15 13:26:21 -0230

    Altered files nto be able to get a single game by its game_id

--- Routes/API/games.js
+++ Routes/API/games.js
@@ -14,4 +14,24 @@ router.get('/', async (req, res) => {
     }
 });
 
+
+// api/actors/:id
+router.get('/:id', async (req, res) => {
+    if(DEBUG) console.log('ROUTE: /api/games/:id GET ' + req.url);
+    try {
+        let aGame = await gamesDal.getGameByGameId(req.params.id); 
+        if (aGame.length === 0) {
+            // log this error to an error log file.
+            res.statusCode = 404;
+            res.json({message: "Not Found", status: 404});
+        }
+        else
+            res.json(aGame);
+    } catch {
+        // log this error to an error log file.
+        res.statusCode = 503;
+        res.json({message: "Service Unavailable", status: 503});
+    }
+});
+
 module.exports = router;
--- Services/pg.games.dal.js
+++ Services/pg.games.dal.js
@@ -20,6 +20,24 @@ var getGames= function() {
   };
 
 
+  var getGameByGameId = function(id) {
+    if(DEBUG) console.log("games.pg.dal.getGameByGameId()");
+    return new Promise(function(resolve, reject) {
+      const sql = "SELECT game_name, developer FROM games WHERE game_id = $1";
+      dal.query(sql, [id], (err, result) => {
+        if (err) {
+          // logging should go here
+          if(DEBUG) console.log(err);
+          reject(err);
+        } else {
+          resolve(result.rows);
+        }
+      }); 
+    }); 
+  };
+
+
   module.exports = {
-    getGames
+    getGames,
+    getGameByGameId
   }
